993fc738b43b708441f7cf0fabe678b4f34fc6fc
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-11-16 22:34:21 +0500

    Add files via upload

---
+++ lib/appwrite_client.ts
@@ -0,0 +1,10 @@
+import { Client , Databases} from "appwrite";
+
+const client = new Client();
+
+client.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
+       .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
+
+       const databases = new Databases(client);
+
+export default client
